e71b545309dda54b970cc0078e13863ebb5e34af
Johan Viancha <johanviancha@wposs.com>
2021-07-10 12:58:56 -0500

    Finalización del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { PaisesService } from './services/paises.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'API-Paises';
+  formulario: FormGroup;
+  paises: any[];
+  constructor(private paisesService: PaisesService) {
+    this.paises = [];
+    this.formulario = new FormGroup({
+      filtro: new FormControl('todos'),
+      valor: new FormControl(''),
+    });
+  }
+
+  ngOnInit(): void {}
+
+  getAll() {
+    this.paisesService
+      .getAll()
+      .then((resolve) => {
+        this.paises = resolve;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  filtrar() {
+    let filter = this.formulario.value.filtro;
+    let value = this.formulario.value.valor;
+    switch (filter) {
+      case 'todos':
+        this.getAll();
+        break;
+      case 'region':
+        this.paisesService
+          .getForRegion(value)
+          .then((resolve) => (this.paises = resolve))
+          .catch((error) => {
+            console.log(error);
+          });
+        break;
+
+      case 'lenguage':
+        this.paisesService
+          .getForLanguage(value)
+          .then((resolve) => (this.paises = resolve))
+          .catch((error) => {
+            console.log(error);
+          });
+        break;
+
+      case 'moneda':
+        this.paisesService
+          .getForMOneda(value)
+          .then((resolve) => (this.paises = resolve))
+          .catch((error) => {
+            console.log(error);
+          });
+
+        break;
+    }
+  }
 }
---
+++ src/app/services/paises.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PaisesService {
+  Url: string;
+
+  constructor(private httpClient: HttpClient) {
+    this.Url = 'https://restcountries.eu/rest/v2';
+  }
+
+  getAll(): Promise<any[]> {
+    return this.httpClient.get<any[]>(`${this.Url}/all`).toPromise();
+  }
+
+  getForRegion(region: string): Promise<any[]> {
+    return this.httpClient
+      .get<any[]>(`${this.Url}/region/${region}`)
+      .toPromise();
+  }
+
+  getForLanguage(language: string): Promise<any[]> {
+    console.log(language);
+    return this.httpClient
+      .get<any[]>(`${this.Url}/lang/${language}`)
+      .toPromise();
+  }
+
+  getForMOneda(moneda: string): Promise<any[]> {
+    return this.httpClient
+      .get<any[]>(`${this.Url}/currency/${moneda}`)
+      .toPromise();
+  }
+}
